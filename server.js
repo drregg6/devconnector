c8de204c664761d7029df75a2e6b7c3bf4063b39
Dave Regg <drregg6@gmail.com>
2019-06-11 13:21:43 -0400

    Added express-validator to user POST route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require('./config/db');
 // Connect database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
