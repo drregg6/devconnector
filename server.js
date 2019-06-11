592f3ca19ddc18a480d959a0a8d334944838d9f1
Dave Regg <drregg6@gmail.com>
2019-06-11 13:03:18 -0400

    Added User model and routes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 7000;
 const connectDB = require('./config/db');
 
 // Connect database
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
