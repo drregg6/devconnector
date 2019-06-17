7899691e763f136fb8d08bbb2b50e132d3b7f130
Dave Regg <drregg6@gmail.com>
2019-06-17 19:59:57 -0400

    Delete profile route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,6 +137,26 @@ router.get('/user/:user_id', async (req, res) => {
     }
     res.status(500).send('Server error');
   }
+});
+
+// @route  DELETE api/profile
+// @desc   Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server error');
+  }
 })
 
 module.exports = router;
