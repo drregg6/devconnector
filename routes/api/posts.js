f8193b8775739b7ef2c7bbc7a9ef816c0606b2d3
Dave Regg <drregg6@gmail.com>
2019-06-18 21:41:30 -0400

    Get all posts, get a post, and delete a post routes created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -19,8 +19,11 @@ router.post('/', [ auth, [
   if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
   try {
+    // grab the entire user object
+    // { _id, name, email, avatar }
     const user = await User.findById(req.user.id).select('-password');
 
+    // create new post using user.name and user.avatar
     const newPost = new Post({
       text: req.body.text,
       name: user.name,
@@ -28,9 +31,71 @@ router.post('/', [ auth, [
       user: req.user.id
     });
 
+    // save and return post
     const post = await newPost.save();
     res.json(post);
   } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); // most recent date
+    res.json(posts);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById( req.params.id );
+
+    if (!post) {
+      return res.status(404).json({ msg: 'No post found' })
+    }
+
+    res.json(post);
+  } catch (e) {
+    if (e.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    console.error(e.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById( req.params.id );
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    
+    // Check if the user deleting the post owns that post
+    if (post.user.toString() !== req.user.id) { // post.user is an ObjectId, not a string
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+  } catch (e) {
+    if (e.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
     console.error(e.message);
     res.status(500).send('Server error');
   }
