08a3294673004bd4e81a15bc57ce0dc351cb5504
Dave Regg <drregg6@gmail.com>
2019-06-25 18:19:27 -0400

    PrivateRoutes will protect auth routes

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+// fetch data with action, bring in with redux state, pass down to other components
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Dashboard = props => {
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard
