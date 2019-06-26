28823d65f1096f254a29a4212dfd61237aedfda7
Dave Regg <drregg6@gmail.com>
2019-06-26 13:12:21 -0400

    Create profile action created and used in CreateProfile Component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,35 @@ export const getCurrentProfile = () => async dispatch => {
     })
   }
 }
+
+// Create or Update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    // Sending data means sending config with the request
+    const config = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (e) {
+    const errors = e.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: e.response.statusText, status: e.response.status }
+    });
+  }
+}
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -42,6 +44,12 @@ const CreateProfile = props => {
     })
   }
 
+  const onSubmit = e => {
+    e.preventDefault();
+
+    createProfile(formData, history);
+  }
+
   return (
     <React.Fragment>
       <h1 className="large text-primary">
@@ -52,7 +60,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -160,9 +168,10 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired
 }
 
 export default connect(
-  null
-)(CreateProfile);
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
