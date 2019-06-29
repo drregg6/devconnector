be2abbd61d567db6e84d569575f5deeb974bd36e
Dave Regg <drregg6@gmail.com>
2019-06-29 11:07:39 -0400

    Profile Reducer and Action populated with get calls

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,6 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   UPDATE_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE
@@ -21,6 +22,18 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
+        loading: false
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     case PROFILE_ERROR:
