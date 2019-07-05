e3d3bf406ea9d13188cff59e06e0c14207538f7d
Dave Regg <drregg6@gmail.com>
2019-07-05 19:43:44 -0400

    Post and Comments created

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from './types';
 import { setAlert } from './alert';
 import axios from 'axios';
@@ -107,6 +109,48 @@ export const getPost = (id) => async dispatch => {
       type: GET_POST,
       payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const res = await axios.put(`/api/posts/comment/${postId}`, formData, config);
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 import { connect } from 'react-redux';
 import { getPost } from '../../actions/post';
@@ -19,12 +21,28 @@ const Post = ({
   useEffect(() => {
     getPost(match.params.id)
   }, [getPost])
-  return loading || post === null ? <Spinner /> : <Fragment>
-    <Link to="/posts" className="btn">
-      Back to Posts
-    </Link>
-    <PostItem post={post} showActions={false} />
-  </Fragment>
+  return (
+    loading || post === null ? <Spinner /> : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => {
+          return (
+            <CommentItem
+              key={comment._id}
+              comment={comment}
+              postId={post._id}
+            />
+          )
+        })}
+      </div>
+    </Fragment>
+    )
+  )
 }
 
 Post.propTypes = {
